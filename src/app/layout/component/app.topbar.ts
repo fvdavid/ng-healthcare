7c685d6f42a22c66e583b3a2fc75cac0bfacb5e0
David Tobing <saddamtbg@gmail.com>
2025-03-07 22:56:31 +0700

    change name

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -33,7 +33,7 @@ import { LayoutService } from '../service/layout.service';
                         />
                     </g>
                 </svg>
-                <span>SAKAI</span>
+                <span>fv Heartcare</span>
             </a>
         </div>
 

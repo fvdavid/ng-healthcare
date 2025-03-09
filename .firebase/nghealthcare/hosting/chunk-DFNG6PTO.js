ff152c0a1afb663ce3099974f6b71f7c9d8196fa
David Tobing <saddamtbg@gmail.com>
2025-03-09 15:59:43 +0700

    remove .firebase

--- .firebase/nghealthcare/hosting/chunk-DFNG6PTO.js
+++
@@ -1,5 +0,0 @@
-import{Aa as y,La as v}from"./chunk-WGFAF6LW.js";import{$ as a,Qb as l,Rb as p,V as r,W as c,bd as g,fb as s,gb as d,gc as m,jb as u,la as i,yb as f}from"./chunk-JKMQL6A7.js";var h=["*"],D=({dt:e})=>`
-    .p-fluid{
-        width:100%
-    }
-`,M={root:"p-fluid"},F=(()=>{class e extends y{name="fluid";classes=M;theme=D;static \u0275fac=(()=>{let t;return function(o){return(t||(t=i(e)))(o||e)}})();static \u0275prov=r({token:e,factory:e.\u0275fac})}return e})();var j=(()=>{class e extends v{_componentStyle=a(F);static \u0275fac=(()=>{let t;return function(o){return(t||(t=i(e)))(o||e)}})();static \u0275cmp=s({type:e,selectors:[["p-fluid"]],hostVars:2,hostBindings:function(n,o){n&2&&f("p-fluid",!0)},features:[m([F]),u],ngContentSelectors:h,decls:1,vars:0,template:function(n,o){n&1&&(l(),p(0))},dependencies:[g],encapsulation:2,changeDetection:0})}return e})(),k=(()=>{class e{static \u0275fac=function(n){return new(n||e)};static \u0275mod=d({type:e});static \u0275inj=c({imports:[j]})}return e})();export{j as a,k as b};
